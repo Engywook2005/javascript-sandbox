2b637f1e7f51f364eca370eee052e8af45ac2e0e
Greg Thorson <Engywook2005@gmail.com>
2024-10-23 08:50:07 -0400

    Annotated mergeSort

--- app/main.js
+++ app/main.js
@@ -1,9 +1,5 @@
 import {iterativeMergeSort} from './algorithms';
 
-let k = 0;
-let arr = new Array(8);
-arr[k++] = "fish";
-
-// const exampleArray = [8, 4, 7, 3, 5, 2, 6, 1];
-// const sortedArray = iterativeMergeSort(exampleArray);
-// console.log(sortedArray);
+const exampleArray = [8, 4, 7, 3, 5, 2, 6, 9, 1];
+const sortedArray = iterativeMergeSort(exampleArray);
+console.log(sortedArray);
